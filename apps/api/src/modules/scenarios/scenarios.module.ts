f07e8beb6b121059678dea4cea40066e5b0361d2
Михаил <mkarych@Mac.IGD_Rostelecom>
2026-07-03 13:39:08 +0500

    Мини фикс

--- apps/api/src/modules/scenarios/scenarios.module.ts
+++ apps/api/src/modules/scenarios/scenarios.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ScenariosController } from './scenarios.controller';
 import { ScenariosService } from './scenarios.service';
 import { ActivityModule } from '../activity/activity.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [ActivityModule],
+  imports: [ActivityModule, AuthModule],
   controllers: [ScenariosController],
   providers: [ScenariosService],
   exports: [ScenariosService],
